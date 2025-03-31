056c1b87c1a822f028716e4062833329acc9d39d
rahma-bh4 <benhassine.rahma4@gmail.com>
2025-03-31 19:02:57 +0100

    user model removed use profiles instead

--- api/models.py
+++ api/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-class User(models.Model):
-    clerk_user_id = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(null=True, blank=True)
-    first_name = models.CharField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_name=models.CharField(max_length=255, null=True, blank=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+# class User(models.Model):
+#     clerk_user_id = models.CharField(max_length=255, unique=True)
+#     email = models.EmailField(null=True, blank=True)
+#     first_name = models.CharField(max_length=255, null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     last_name=models.CharField(max_length=255, null=True, blank=True)
+#     phone_number = models.CharField(max_length=20, blank=True, null=True)
 
-    def __str__(self):
-        return self.clerk_user_id
+#     def __str__(self):
+#         return self.clerk_user_id
     
 
     
