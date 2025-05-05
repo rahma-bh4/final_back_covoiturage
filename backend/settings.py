fb601fa9e794d38e5560f1ada12064ed4b86b43e
rahma-bh4 <benhassine.rahma4@gmail.com>
2025-05-06 00:06:30 +0100

    fixing trip details

--- backend/settings.py
+++ backend/settings.py
@@ -136,8 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files (Uploaded files)
+VOITURES_ROOT = os.path.join(BASE_DIR, 'voitures')
+VOITURES_URL = '/voitures/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
